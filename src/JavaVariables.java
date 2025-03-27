6ec65c820c101df06c737d63effbd28ebad20d11
le trung duong <duong.lt@wfc.vn>
2025-03-27 15:49:59 +0700

    add 2

---
+++ src/JavaVariables.java
@@ -0,0 +1,18 @@
+public class JavaVariables {
+    public static void main(String[] args){
+        int myNum = 5;
+        final int myNum2 = 10;
+        char myChar = 'a';
+        boolean myBool = true;
+        String myString = "abc";
+        String lastName = "a";
+        String firstName = "b";
+
+        int age = 20;
+        int age2 = 30;
+        int age3 = 40;
+
+        System.out.println(lastName + firstName);
+        System.out.println("this is" + myString);
+    }
+}
